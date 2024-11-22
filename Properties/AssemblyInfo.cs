e2a0d67b8894185f53ed26b14937edca9ef883b5
Mario Romero Ávila <damntry@hotmail.com>
2024-11-22 20:08:34 +0100

    - Set versions of the utility projects - Small name changes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using AssemblyInfo;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -32,5 +32,13 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(Info.Version)]
+[assembly: AssemblyFileVersion(Info.Version)]
+
+namespace AssemblyInfo {
+
+	internal static class Info {
+		internal const string Version = "0.1.5";
+	}
+
+}
